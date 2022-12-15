63f5036f736d6ccfdf0637baa272a6389c8e442c
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-15 19:07:36 +0000

    fixed bug

--- modes.c
+++ modes.c
@@ -1,5 +1,24 @@
-sjdbmbsjhdbjsgdvjgsdvjmhsbjdh
-sdjgsdgvjbdgvjgdvsgdujgbkabgjbm
-asgdvykedgaeihyreuhdnsdhdk
-dhb,udhpoedjmv,cdncl
+#include "monty.h"
+
+/**
+ * stack_mode - change to stack LIFO format
+ *
+ * @cmd: struct containing useful variables
+ */
+
+void stack_mode(cmd_t *cmd)
+{
+	*cmd->mode = 0;
+}
+
+/**
+ * queue_mode - change to queue FIFO format
+ *
+ * @cmd: struct containing useful variables
+ */
+
+void queue_mode(cmd_t *cmd)
+{
+	*cmd->mode = 1;
+}
 
