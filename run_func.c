d0ba4a2003fd910dd6b475b63f641342d62f8cf6
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-15 19:16:02 +0000

    fixed bug

--- run_func.c
+++ run_func.c
@@ -1,4 +1,43 @@
-kshdksvajsvdjsgvdjasjvjsdhjs
-sGHVSGvdjsdbjdhjsdsd
-sdjhbkygdygzjbzkdk,djldkf
-dzkdbhzsdsdjzlund mjsegdlseukd
+#include "monty.h"
+
+/**
+ * run_func - run's specified function
+ * @cmd: data to pass to function
+ */
+void run_func(cmd_t *cmd)
+{
+	instruction_t funcs[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", divide},
+		{"mul", mul},
+		{"mod", mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{"stack", stack_mode},
+		{"queue", queue_mode},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (funcs[i].opcode)
+	{
+		if (strcmp(cmd->op, funcs[i].opcode) == 0)
+		{
+			funcs[i].f(cmd);
+			return;
+		}
+		i++;
+	}
+
+	fprintf(stderr, "L%d: unknown instruction %s\n", cmd->line_number, cmd->op);
+	exit(EXIT_FAILURE);
+}
